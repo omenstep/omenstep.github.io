bb8deaf65f382dea5ff460b7970f145edc20053c
Sergey Tsvetoshenko <tsvetoshenko@gmail.com>
2016-03-11 16:06:56 +0300

    taxi-2

---
+++ taxi-2/js/scripts.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    //$('.form-button').magnificPopup({
+    //    type: 'inline',
+    //    preloader: false,
+    //    focus: '#name',
+    //
+    //    callbacks: {
+    //        beforeOpen: function () {
+    //            if ($(window).width() < 700) {
+    //                this.st.focus = false;
+    //            } else {
+    //                this.st.focus = '#name';
+    //            }
+    //        }
+    //    }
+    //});
+
+    $('#phone').inputmask("+7 (999) 999-99-99");
+    $('#phone-mask').inputmask("+7 (999) 999-99-99");
+
+    $(function () {
+        $('#date').datetimepicker({
+            locale: 'ru',
+            format: 'DD/MM/YYYY',
+            widgetPositioning: {
+                horizontal: 'right',
+                vertical: 'bottom'
+            }
+        });
+    });
+    $(function () {
+        $('#time').datetimepicker({
+            locale: 'ru',
+            format: 'HH:mm',
+            widgetPositioning: {
+                horizontal: 'right',
+                vertical: 'bottom'
+            }
+        });
+    });
+
+    //$("#callback-form").validate({
+    //    rules: {
+    //        phone: {
+    //            required: true,
+    //            minlength: true
+    //        }
+    //    },
+    //    messages: {
+    //        phone: {
+    //            required: "Вы не ввели номер!",
+    //        }
+    //    },
+    //    submitHandler: function (form) {
+    //        /*$.ajax({
+    //            type: "POST",
+    //            url: "form.php",
+    //            data: $(form).serialize(),
+    //            success: function () {*/
+    //                $(form).html("<div id='message'></div>");
+    //                $('#message').html("<h2>Ваша заявка отправлена!</h2>")
+    //                    .hide()
+    //                    .fadeIn(1500, function () {
+    //                        $('#message').append("<img id='checkmark' src='images/Ok.png' />");
+    //                    //});
+    //            //}
+    //        });
+    //        return false;
+    //    }
+    //});
+});
